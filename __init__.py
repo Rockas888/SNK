de92ec01f85e5e7fe5cd5abfad050355bc8b2d64
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2023-10-12 17:49:52 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("10499690", default=None, cast=int)
+API_HASH = config("87d0414dc159c10225cac921edde640a", default=None)
+BOT_TOKEN = config("6507710656:AAFW_VZHa8QqQrn8E_Xs2IrF5q0IVFhSWsI", default=None)
+SESSION = config("BQCt6m-i4mX2vp-AeHPzCqfmyFw6AJKCXgc1tpwx7RiDUx4gZK1V_55H_i_Oq-mnvBkB1iePZD1HA7A0ZczvdcP7qwU-D-jqzvfRZxgbijof4WZcVunCebN3rotMyPEk_PwgRC8JycS4Z9em0cdARaAvTjsUBzo1bXZsPFTtAwzlaSNCSlV6VANM_BzooJW4T2eOEcVRgnyTEB_4RXtO8BnleumMvWMno4NiGtUppQC3uLKLb1CLuyim6VvQNdugHUz-moDX-dDssnXjevxNITtnz3mnwH1y8BEuWUc_vctEr87uouZV54PM6yfXaKNWfL1pK_A4-1T5dC-phN1VBEwZfKhXOAA", default=None)
+FORCESUB = config("restrictwala", default=None)
+AUTH = config("2091407160", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
