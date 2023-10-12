67347e79e5910d28347cbec6519438b49219d367
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2023-10-12 17:59:14 +0530

    Update __init__.py in

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+
 #Github.com/Vasusen-code
 
 from pyrogram import Client
@@ -19,9 +20,9 @@ SESSION = config("BQCt6m-i4mX2vp-AeHPzCqfmyFw6AJKCXgc1tpwx7RiDUx4gZK1V_55H_i_Oq-
 FORCESUB = config("restrictwala", default=None)
 AUTH = config("2091407160", default=None, cast=int)
 
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+bot = TelegramClient('6507710656:AAFW_VZHa8QqQrn8E_Xs2IrF5q0IVFhSWsI', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
+userbot = Client("saverestricted", BQCt6m-i4mX2vp-AeHPzCqfmyFw6AJKCXgc1tpwx7RiDUx4gZK1V_55H_i_Oq-mnvBkB1iePZD1HA7A0ZczvdcP7qwU-D-jqzvfRZxgbijof4WZcVunCebN3rotMyPEk_PwgRC8JycS4Z9em0cdARaAvTjsUBzo1bXZsPFTtAwzlaSNCSlV6VANM_BzooJW4T2eOEcVRgnyTEB_4RXtO8BnleumMvWMno4NiGtUppQC3uLKLb1CLuyim6VvQNdugHUz-moDX-dDssnXjevxNITtnz3mnwH1y8BEuWUc_vctEr87uouZV54PM6yfXaKNWfL1pK_A4-1T5dC-phN1VBEwZfKhXOAA, 87d0414dc159c10225cac921edde640a, 10499690) 
 
 try:
     userbot.start()
